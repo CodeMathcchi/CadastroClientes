3bd27d313921c8fd860fcf60bcd0b7d18e8ee8d1
Matheus Santos <53152862+SantosMatheusGR@users.noreply.github.com>
2020-10-12 13:42:38 -0300

    Ajustes front

--- CadastroClientes/src/app/cadastro/cadastro.component.ts
+++ CadastroClientes/src/app/cadastro/cadastro.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
--- CadastroClientes/src/app/clientes/clientes.component.ts
+++ CadastroClientes/src/app/clientes/clientes.component.ts
@@ -12,25 +12,26 @@ export class ClientesComponent implements OnInit {
 
   public clienteForm: FormGroup;
 
-  titulo = "Clientes"; 
+  titulo = 'Clientes';
 
   public clienteSelecionado: Cliente;
+  public clienteEmEdicao: Cliente;
 
   clientes = [
     {
       id : 1,
-      nome : "Patrick",
-      dataNascimento : "27/12/1999",
-      sexo : "M",
+      nome : 'Patrick',
+      dataNascimento : '27/12/1999',
+      sexo : 'M',
       rua : 'Rua Pioneiro Herculano Ferreira',
       numero : '556',
       bairro : 'Conj. Hab. Sanenge III'
     },
     {
       id : 2,
-      nome : "Matheus",
-      dataNascimento : "14/12/1995",
-      sexo : "M",
+      nome : 'Matheus',
+      dataNascimento : '14/12/1995',
+      sexo : 'M',
       rua : 'Avenida Londrina',
       numero : '1598',
       bairro : 'Conjunto Aeroporto'
@@ -38,9 +39,20 @@ export class ClientesComponent implements OnInit {
   ];
 
   modalRef: BsModalRef;
- 
-  openModal(template: TemplateRef<any>) {
+
+  openModal(template: TemplateRef<any>, cliente: Cliente): void {
     this.modalRef = this.modalService.show(template);
+    this.editarCliente(cliente);
+  }
+
+  closeModal(): void{
+    this.modalRef.hide();
+    this.clienteEmEdicao = null;
+  }
+
+  editarCliente(cliente: Cliente): void{
+    this.clienteEmEdicao = cliente;
+    this.clienteForm.patchValue(cliente);
   }
 
   constructor(private fb: FormBuilder, private modalService: BsModalService) {
@@ -50,16 +62,22 @@ export class ClientesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  selecionarCliente(cliente: Cliente) {
+  selecionarCliente(cliente: Cliente): void {
     this.clienteSelecionado = cliente;
     this.clienteForm.patchValue(cliente);
   }
 
-  public voltar(){
+  public voltar(): void {
     this.clienteSelecionado = null;
+    if (this.clienteEmEdicao){
+      this.clienteEmEdicao = null;
+    }
+  }
+  public cancelar(): void{
+    this.clienteEmEdicao = null;
   }
 
-  criarForm(){
+  criarForm(): void {
     this.clienteForm = this.fb.group({
       nome : ['', Validators.required],
       dataNascimento : ['', Validators.required],
@@ -70,7 +88,7 @@ export class ClientesComponent implements OnInit {
     });
   }
 
-  clienteSubmit(){
-    console.log(this.clienteForm.value)
+  clienteSubmit(): void{
+    console.log(this.clienteForm.value);
   }
 }
